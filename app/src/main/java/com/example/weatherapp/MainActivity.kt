ea7eb765ea5ab26a7c3804dc75a3e1d5959b9754
gilles loriquer <gilles.loriquer@gfi.fr>
2019-10-04 10:39:43 +0200

    Création de l'arborescence de package et des fragments Création et implémentation de la toolbar Création et implémentation de la bottom navigation Mise en place des éléments nécéssaires à la navigation (NavHostFragment etc.) Définition du style et des couleurs Trop gros commit...

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.weatherapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Sets the toolbar as the app bar for the activity
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        // Gets the NavController from the activity
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        // TOOLBAR
+        // Sets up the ActionBar returned by AppCompatActivity.getSupportActionBar() for use with a NavController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        // BOTTOM NAVIGATION
+        // Sets up a BottomNavigationView for use with a NavController
+        // Extension function => NavigationUI.setupWithNavController(bottom_nav, navController)
+        bottom_nav.setupWithNavController(navController)
+    }
+
+    // This method is called whenever the user chooses to navigate Up within your application's activity hierarchy
+    // from the action bar.
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
---
+++ app/src/main/java/com/example/weatherapp/ui/weather/current/CurrentWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.weatherapp.ui.weather.current
+
+import androidx.lifecycle.ViewModel;
+
+class CurrentWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
